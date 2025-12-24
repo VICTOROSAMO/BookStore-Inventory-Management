84196519587d02995d29b889a7ff2b7b46756d69
Osamo <osamovicky2021@gmail.com>
2025-12-25 02:02:58 +0300

    BookStore Inventory

--- models/books.model.js
+++ models/books.model.js
@@ -4,15 +4,21 @@ const mongoose = require("mongoose")
 const bookSchema = mongoose.Schema({
     bookName: {
         type: String,
-        required: true
+        required: [true, "Book name is required!"],
+        minlength: [5, "Book name must be atleast 5 characters long"],
+        maxlength: [100, "Book length cannot exceed 100 characters long"]
     },
     countInStock: {
         type: Number,
-        required: true
+        required: [true, "Stock count is required!"],
+        min: [1, "Stock count cannot be less that 1"],
+        max: [255, "Stock count cannot exceed 255"]
     },
     price: {
         type: Number,
-        required: true
+        required: [true, "Stock count is required!"],
+        min: [1, "Price cannot be negative"],
+        max: [255, "Price cannot exceed 10,000"]
     }
 })
 
