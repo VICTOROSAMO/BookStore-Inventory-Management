8723dde0a7be437fa29541dd12a859f71da4768b
Osamo <osamovicky2021@gmail.com>
2026-01-03 20:54:45 +0300

    BookStore Inventory Management

--- routes/books.route.js
+++ routes/books.route.js
@@ -2,7 +2,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const BookModel = require("../models/books.model")
-const { body, validationResult} = require("express-validator")
+const { body, validationResult, param} = require("express-validator")
 
 const router = express.Router()
 
@@ -86,8 +86,35 @@ router.delete("/:id", async (req, res) => {
 })
 
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", 
+    
+ [
+    param("id").isMongoId().withMessage("Invalid Book Id"),  
+    
+    body("bookName")
+      .optional()
+      .isLength({min:5, max:100}).withMessage("Book name must be between 5 to 100 characters."),
+
+      body("price")
+      .optional()
+      .isFloat({min:1, max:1000}).withMessage("price must be between 1 and 1000"),
+      
+      body("countInStock")
+      .optional()
+      .isInt({min:1, max:255}).withMessage("Stock count must be between 1 and 255"),
+
+      body("image")
+      .optional()
+      .isURL().withMessage("Image must be a valid URL")
+
+    ]
+    ,async (req, res) => {
     try {
+         const errors = validationResult(req)  
+    
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
         const { id } = req.params
         const updatedBook = await BookModel.findByIdAndUpdate(id, req.body, {new: true})
 
